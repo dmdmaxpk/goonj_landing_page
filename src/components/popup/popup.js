cf5ea80dfecbf82356745a92fcedfe46780012fe
Taha <tbk454@gmail.com>
2020-01-16 17:14:18 +0500

    Font added, grid removed

--- src/components/popup/popup.js
+++ src/components/popup/popup.js
@@ -12,16 +12,20 @@ export default class  Popup extends React.Component {
     return (
         <div class="popup">
           <div class="popup_content">
-            <div class="popup_row_1">
-              <img src={require("../../assets/logo.png")} />
+            {/* <br /> */}
+            <div class="">
+              <img className="goonj_logo" src={require("../../assets/logo.png")} />
             </div>
-            <div class="popup_row_2">
-              <p className="popup_text_1">TO START YOUR FREE</p>
-              <p className="popup_text_2">ONE DAY TRIAL CLICK ON</p>
-              <p className="popup_text_3">SUBSCRIBE</p>
+            <div class="">
+              <div className="popup_text_1">TO START YOUR FREE</div>
+              <div className="popup_text_2">ONE DAY TRIAL CLICK ON</div>
+              <div className="popup_text_3">SUBSCRIBE</div>
             </div>
-            <div class="popup_row_3">
-              <button class="button">Subscribe</button>
+            <div class="">
+              <button className="button">
+                <div style={{float: "left", marginLeft: "8%"}}>SUBSCRIBE NOW</div>
+                <img src={require("../../assets/t-logo.png")} style={{float: "right", height: "20px", marginRight: "8%"}} />
+              </button>
             </div>
           </div>
         </div>
